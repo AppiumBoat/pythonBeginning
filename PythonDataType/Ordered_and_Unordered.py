99c05477c1933b9df33bcac103c913a55a177b5d
Binesh <appiumtestbinesh@gmail.com>
2022-08-24 01:43:40 +0530

    Updates

---
+++ PythonDataType/Ordered_and_Unordered.py
@@ -0,0 +1,17 @@
+# Examples of ordered and unordered collections
+
+letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+string_letters = str(letters)
+lists_letters = list(letters)
+tuples_letters = tuple(letters)
+sets_letters = set(letters)
+
+
+print("String: ", string_letters)
+print() # for new line
+# Ordered collection of data
+print("Lists: ", lists_letters)
+print() # for new line
+# Unordered collection of data
+print("Sets: ", sets_letters)
